a86c4aa5291922bdd6bf4325889a5b0c7253beae
Piervincenzo Ventrella <piervincenzov@gmail.com>
2017-05-17 16:28:40 +0200

    create Board class

---
+++ src/main/java/it/polimi/ingsw/GC_36/model/Board.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.GC_36.model;
+
+public class Board {
+
+	Die[] dice;
+	TurnOrder turnOrder;
+	Player[] players;
+	DeckSet[] deckSets;
+
+
+	public Board() {
+		//TODO
+	}
+
+	public void initialize() {
+		//TODO
+	}
+
+	public void clean() {
+		//TODO
+	}
+
+	public DeckSet getDeckSet(int periodNumber) {
+		//TODO
+	}
+
+	public TurnOrder getTurnOrder() {
+		//TODO
+	}
+}
